728deaa9e6a8d3862b27897d2e323bd32c25dc11
Thais Martins <contato@thaismartins.co>
2020-04-17 18:11:42 -0300

    Adicionando styled components

---
+++ src/layouts/global.js
@@ -0,0 +1,62 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+
+    *:focus {
+      outline: 0;
+    }
+  }
+
+  html {
+    width: 100vw;
+    overflow-x: hidden;
+    -webkit-overflow-scrolling: touch;
+  }
+  
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+    scroll-behavior: smooth;
+  }
+
+  body {
+    font-family: 'Open Sans', sans-serif;
+    font-style: normal;
+    font-stretch: normal;
+    font-weight: 400;
+    line-height: normal;
+    letter-spacing: normal;
+    background: #fff;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    color: #3e3e3e;
+    padding-top: 100px;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  input,
+  textarea,
+  select {
+    font-family: 'Open Sans', sans-serif;
+    font-style: normal;
+    font-stretch: normal;
+    font-weight: 400;
+    line-height: normal;
+    letter-spacing: normal;
+  }
+`;
+
+export default GlobalStyle;
