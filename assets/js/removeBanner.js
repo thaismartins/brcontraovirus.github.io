67ef226715346741f2979da62b56ca2d31890af5
Thais Martins <contato@thaismartins.co>
2020-04-16 21:24:27 -0300

    Initial commit

---
+++ assets/js/removeBanner.js
@@ -0,0 +1,6 @@
+
+window.onload = () => {
+    let bannerNode = document.querySelector('[alt="www.000webhost.com"]').parentNode.parentNode;
+    bannerNode.parentNode.removeChild(bannerNode);
+ }
+ 
