041d5e69752fb5e29c7aaefbb690287eac2aecb1
Thais Martins <contato@thaismartins.co>
2020-04-20 11:50:54 -0300

    Ajustando SEO

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -1,21 +1,9 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Helmet } from 'react-helmet'
+import { useStaticQuery, graphql } from 'gatsby'
 
-import React from "react"
-import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
-
-const SEO = ({ 
-  description,
-  meta,
-  title,
-  lang
-}) => {
+const SEO = ({ description, meta, title, lang }) => {
   const { site } = useStaticQuery(
     graphql`
       query {
