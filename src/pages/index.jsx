e4de28620aaccbc12f3fe1aa47878cf41ceddfcf
Thais Martins <contato@thaismartins.co>
2020-04-20 20:34:40 -0300

    Adicionando banner da home

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '@layouts/Default'
+
 import SEO from '@components/SEO'
+import BannerHome from '@templates/BannerHome'
 
 const IndexPage = () => (
   <Layout>
     <SEO title='Home' />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to='/page-2/'>Go to page 2</Link>
+    <BannerHome />
   </Layout>
 )
 
