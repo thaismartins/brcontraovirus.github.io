5756cd95879ec1a99144f325aea44efc39c23be8
Thais Martins <contato@thaismartins.co>
2020-04-20 22:14:29 -0300

    Removendo className

--- src/components/Center/index.jsx
+++ src/components/Center/index.jsx
@@ -3,17 +3,10 @@ import PropTypes from 'prop-types'
 
 import { Container } from './styles'
 
-const Center = ({ children, className }) => (
-  <Container className={className}>{children}</Container>
-)
+const Center = ({ children }) => <Container>{children}</Container>
 
 Center.propTypes = {
   children: PropTypes.node.isRequired,
-  className: PropTypes.string,
-}
-
-Center.defaultProps = {
-  className: '',
 }
 
 export default Center
