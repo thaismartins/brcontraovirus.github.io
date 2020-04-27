b379bf9de4446d770359d1d6f90d68c6d7e63b0e
Thais Martins <contato@thaismartins.co>
2020-04-26 22:03:51 -0300

    Ajustando uso do componente de reportagens

--- src/templates/Press/index.jsx
+++ src/templates/Press/index.jsx
@@ -2,31 +2,28 @@ import React from 'react'
 import Headline from '@components/Headline'
 
 const Press = () => {
-    return (
-        <div>
-            <Headline
-                image='globo.png'
-                alt='globo'
-                text='Com falta de equipamentos de proteção individual, hospitais contam com doações da sociedade civil.'
-                buttonText='Saiba mais'
-                linkTo=''
-            />
-            <Headline
-                image='g1.png'
-                alt='g1'
-                text='Brasileira cria rede para imprimir em 3D equipamentos de UTI usados no tratamento contra o coronavírus.'
-                buttonText='Saiba mais'
-                linkTo=''
-            />
-            <Headline
-                image='sbt.jpeg'
-                alt='sbt'
-                text='Mais de cem empresas poderão usar as linhas de montagem para fabricar respiradores hospitalares.'
-                buttonText='Saiba mais'
-                linkTo=''
-            />
-        </div>
-    )
+  return (
+    <div>
+      <Headline
+        image='globo.png'
+        title='globo'
+        text='Com falta de equipamentos de proteção individual, hospitais contam com doações da sociedade civil.'
+        link='http://google.com'
+      />
+      <Headline
+        image='g1.png'
+        title='g1'
+        text='Brasileira cria rede para imprimir em 3D equipamentos de UTI usados no tratamento contra o coronavírus.'
+        link='http://google.com'
+      />
+      <Headline
+        image='sbt.jpeg'
+        title='sbt'
+        text='Mais de cem empresas poderão usar as linhas de montagem para fabricar respiradores hospitalares.'
+        link='http://google.com'
+      />
+    </div>
+  )
 }
 
 export default Press
