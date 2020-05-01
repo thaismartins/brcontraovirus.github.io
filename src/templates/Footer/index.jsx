7606da79358610933114d9c85fbf889b10e536e2
Thais Martins <contato@thaismartins.co>
2020-05-01 15:43:35 -0300

    Adicionando footer

--- src/templates/Footer/index.jsx
+++ src/templates/Footer/index.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 import Center from '@components/Center'
 import Image from '@components/Image'
+import Socials from '@components/Socials'
 
-import { Container, Logo } from './styles'
+import {
+  Container,
+  Logo,
+  Details,
+  Address,
+  Email,
+  Phones,
+  Phone,
+} from './styles'
 
 const Header = () => {
   return (
@@ -12,6 +21,22 @@ const Header = () => {
         <Logo>
           <Image src='logo.png' alt='Brasil contra o Vírus' />
         </Logo>
+
+        <Details>
+          <Address>
+            Av Pres. Castelo Branco, 6061 <br />
+            São Paulo, SP
+          </Address>
+          <Email href='mailto:brcontraovirus@gmail.com'>
+            brcontraovirus@gmail.com
+          </Email>
+          <Phones>
+            <Phone>+55 12 98120 7722</Phone>
+            <Phone>+55 11 99136 2636</Phone>
+            <Phone>+55 11 97281 1999</Phone>
+          </Phones>
+          <Socials />
+        </Details>
       </Center>
     </Container>
   )
