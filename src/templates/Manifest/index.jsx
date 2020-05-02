230e6e7c15a8e6d4c8b51e39e1a20be4a77c847f
Thais Martins <contato@thaismartins.co>
2020-05-01 21:20:40 -0300

    Ajustando estilo

--- src/templates/Manifest/index.jsx
+++ src/templates/Manifest/index.jsx
@@ -8,13 +8,13 @@ import { Container, Title, Text } from './styles'
 const Manifest = () => {
   return (
     <Container>
-      <Background color='purple' full>
+      <Background color='darkPurple' full>
         <Center>
           <Title>Manifesto</Title>
           <Text>
             Através do trabalho organizado e colaborativo entre profissionais,
             academicos, cientistas, empresas, instituições públicas, privadas e
-            o governo, acreditamos que podemos
+            o governo, acreditamos que podemos{` `}
             <b>
               auxiliar o Setor da Saúde e combater a epidemia de COVID-19 no
               Brasil e no Mundo
@@ -22,15 +22,15 @@ const Manifest = () => {
             , através de Tecnologias emergentes e uma rede de colaboradores.
             Acreditamos que que as ações da Rede Brasil contra o Vírus têm a
             capacidade de alterar a maneira como
-            <b> cientistas, empresas, makers e o poder público</b>
+            <b> cientistas, empresas, makers e o poder público </b>
             se relacionam a fim de equacionar as demandas da sociedade
             contemporânea. Acreditamos que a
-            <b> colaboração mútua na sociedade</b>
-            ,assim como as ferramentas modernas que libertam, como softwares e
-            sistemas open-source, contribuem substancialmente para a difusão do
-            conhecimento e oportunidade entre todos, especialmente para a
-            geração de renda, empregos e empreendedores sem a capacidade
-            financeira da indústria. Por fim, acreditamos que a
+            <b> colaboração mútua na sociedade</b>, assim como as ferramentas
+            modernas que libertam, como softwares e sistemas open-source,
+            contribuem substancialmente para a difusão do conhecimento e
+            oportunidade entre todos, especialmente para a geração de renda,
+            empregos e empreendedores sem a capacidade financeira da indústria.
+            Por fim, acreditamos que a
             <b> união da Tecnologia, conhecimento e colaboração </b>
             podem melhorar o planeta e as pessoas.
           </Text>
