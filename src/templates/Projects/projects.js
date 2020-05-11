da0ca0fb7ac8731932a0abca5fc82819fc47198f
Thais Martins <contato@thaismartins.co>
2020-05-11 13:36:06 -0300

    Adicionando conteúdo dos projetos

--- src/templates/Projects/projects.js
+++ src/templates/Projects/projects.js
@@ -4,10 +4,8 @@ const projects = [
     category: 'EPI',
     categoryType: 'epis',
     images: [
-      'faceshield/faceshield.jpg',
-      'faceshield/faceshield.jpg',
-      'faceshield/faceshield.jpg',
-      'faceshield/faceshield.jpg',
+      'projects/faceshield-3d/faceshield.jpg',
+      'projects/faceshield-3d/faceshield-2.jpg',
     ],
   },
   {
@@ -15,10 +13,8 @@ const projects = [
     category: 'EPI',
     categoryType: 'epis',
     images: [
-      'faceshield/faceshield.jpg',
-      'faceshield/faceshield.jpg',
-      'faceshield/faceshield.jpg',
-      'faceshield/faceshield.jpg',
+      'projects/faceshield-laser/faceshield.jpg',
+      'projects/faceshield-laser/faceshield-2.jpg',
     ],
   },
   {
@@ -26,22 +22,45 @@ const projects = [
     category: 'Manutenção',
     categoryType: 'manutencao',
     images: [
-      'faceshield/faceshield.jpg',
-      'faceshield/faceshield.jpg',
-      'faceshield/faceshield.jpg',
-      'faceshield/faceshield.jpg',
+      'projects/videolaringoscopio/videolaringo.jpg',
+      'projects/videolaringoscopio/videolaringo-2.jpg',
     ],
   },
   {
     title: 'Divisor de Ventilador',
     category: 'Dispositivos',
     categoryType: 'dispositivos',
-    images: [
-      'faceshield/faceshield.jpg',
-      'faceshield/faceshield.jpg',
-      'faceshield/faceshield.jpg',
-      'faceshield/faceshield.jpg',
-    ],
+    images: ['projects/divisor-ventilador/divisor.jpg'],
+  },
+  {
+    title: 'Trion Clip Shield',
+    category: 'EPI',
+    categoryType: 'epis',
+    images: ['projects/trion/trion.jpg'],
+  },
+  {
+    title: 'Máscara',
+    category: 'EPI',
+    categoryType: 'epis',
+    images: ['projects/mascara/mascara.jpg'],
+  },
+  {
+    title: 'Open Ventilator Spartan',
+    category: 'Dispositivos',
+    categoryType: 'dispositivos',
+    images: ['projects/spartan/spartan.jpg'],
+  },
+  {
+    title: 'Open Lung',
+    category: 'Dispositivos',
+    categoryType: 'dispositivos',
+    images: ['projects/openlung/openlung.jpg'],
+  },
+  {
+    title: 'Valvula PEEP',
+    category: 'Manutenção',
+    categoryType: 'manutencao',
+    images: ['projects/valvula/valvula.jpg'],
   },
 ]
 
